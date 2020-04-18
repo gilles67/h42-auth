468ab0166b2bf09641b9ee646179cfe4e985b784
Gilles GAUVENET <gilles.gauvenet@gmail.com>
2020-04-18 11:42:58 +0200

    Enable gunicorn logging

--- h42auth/__main__.py
+++ h42auth/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 import os.path
 app_path = os.path.abspath(os.path.join(os.path.basename(__file__), '..'))
 sys.path.append(app_path)
@@ -6,4 +7,8 @@ sys.path.append(app_path)
 from h42auth import app
 
 if __name__ == '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    if gunicorn_logger:
+        app.logger.handlers = gunicorn_logger.handlers
+        app.logger.setLevel(gunicorn_logger.level)
     app.run(host="0.0.0.0", debug=True)
--- prod-app.sh
+++ prod-app.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x
 cd /app/h42-auth
-gunicorn -w 4 -b 0.0.0.0:5000 h42auth:app
+gunicorn -w 4 -b 0.0.0.0:5000 --access-logfile- h42auth:app
